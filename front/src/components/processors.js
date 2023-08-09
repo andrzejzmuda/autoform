74462e3f7799281a25285278f07283e466ac5bbe
andrzejzmuda <andrzejzmuda@hotmail.com>
2023-08-09 19:14:04 +0200

    branch testing

--- front/src/components/processors.js
+++ front/src/components/processors.js
@@ -3,6 +3,9 @@ import React from "react";
 
 export default function Processors() {
     return (
-        <h2>Processors</h2>
+        <div>
+            <h2>Processors</h2>
+            <h3>here comes some list</h3>
+        </div>
     );
 }
